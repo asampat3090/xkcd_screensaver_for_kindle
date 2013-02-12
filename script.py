90a1c5e36c878007851592eb07459d850c5344b3
Anand Sampat <asampat3090@gmail.com>
2013-02-11 22:50:32 -0800

    deleted all images

--- script.py
+++ script.py
@@ -5,7 +5,7 @@ destinationFolder = "/Users/asampat3090/Desktop/Programming/xkcd/Images" # Full 
 pattern = re.compile("img.*src=.*title=.*alt=.*")
 
 nrComics = 449 # Number of comics to retrieve
-for i in xrange(1, nrComics + 1):
+for i in xrange(77, nrComics + 1):
     site = urllib.urlopen("http://www.xkcd.com/" + str(i) + "/") # Gets the site
     contentLine = None
     for line in site.readlines(): # Reads the source
@@ -41,29 +41,36 @@ for i in xrange(1, nrComics + 1):
     # Expand image to cover the largest possible area
     # First create a canvas
     canvas_img=Image.new('RGB',(600,800),(255,255,255))
-
+    
     copied=0
 
     img_full_name = img_name + ".gif"
-    xkcd_img = Image.open(img_full_name)
+    xkcd_img = Image.open("Images/" + img_full_name)
     # determine which orientation is best for comic and paste accordingly
     # xkcd_img.show()
     if xkcd_img.size[0]>xkcd_img.size[1]:
         temp_1=xkcd_img.rotate(90)
         # temp_1.show()
-        temp_1.save(img_full_name)
+        temp_1.save("Images/"+img_full_name)
         # resize the image to fit the screen while maintaining the aspect ratio.
         new_to_old_ratio=float(775)/float(temp_1.size[1])
         if temp_1.size[0]*new_to_old_ratio>600:
             temp_2=temp_1.resize((600,int(math.floor(float(temp_1.size[1]*600)/float(temp_1.size[0])))))
+            
+            temp_2.save("Images/"+img_full_name)
+            
             # now paste this temp2 to the 600x800 canvas
             canvas_img.paste(temp_2,(0,25+int(float(775-float(temp_1.size[1]*600)/float(temp_1.size[0]))/float(2))))
+            
+            canvas_img.save("Images/"+img_full_name)
             print 1
         else:
             temp_2=temp_1.resize((int(math.floor(float(temp_1.size[0])*new_to_old_ratio)),775))
+            temp_2.save("Images/"+img_full_name)
             # temp_2.show()
             # now paste this temp2 to the 600x800 canvas
-            canvas_img.paste(temp_2,(int(float(600-float(temp_1.size[0])*new_to_old_ratio)/float(2)),25)) 
+            canvas_img.paste(temp_2,(int(float(600-float(temp_1.size[0])*new_to_old_ratio)/float(2)),25))
+            canvas_img.save("Images/"+img_full_name)
             print 2
          
     else:
@@ -73,24 +80,28 @@ for i in xrange(1, nrComics + 1):
         if xkcd_img.size[1]*new_to_old_ratio>775:
     
             temp_2=xkcd_img.resize((int(math.floor(float(xkcd_img.size[0])*float(775))/float(xkcd_img.size[1])),775))
+            temp_2.save("Images/"+img_full_name)
             #temp_2.show() 
             # now paste this temp2 to the 600x800 canvas
             canvas_img.paste(temp_2,(int(float(600-float(xkcd_img.size[0]*775)/float(xkcd_img.size[1]))/float(2)),25))
+            canvas_img.save("Images/"+img_full_name)
             print 3
         else:
             temp_2=xkcd_img.resize((600,int(math.floor(float(xkcd_img.size[1])*new_to_old_ratio))))
+            temp_2.save("Images/"+img_full_name)
             print xkcd_img.size
             print new_to_old_ratio
             print temp_2.size
             # now paste this temp2 to the 600x800 canvas
             canvas_img.paste(temp_2,(0,25+int(float(775-float(xkcd_img.size[1])*new_to_old_ratio)/float(2))))
+            canvas_img.save("Images/"+img_full_name)
             print 4
     
         
     
     # Add caption to image
     
-    pathz = "/Users/asampat3090/Desktop/Programming/xkcd/Images" + img_full_name
+    pathz = "/Users/asampat3090/Desktop/Programming/xkcd/Images/" + img_full_name
     if os.path.exists(pathz):
         namez = img_full_name[:-4]
         d = ImageDraw.Draw(canvas_img)
